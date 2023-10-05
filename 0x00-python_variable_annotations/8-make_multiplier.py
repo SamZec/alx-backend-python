04a227dad805ea0d95d802cf10bb53b6e2dcfe70
SamZec <samzec4@gmail.com>
2023-10-05 17:25:10 +0000

    8. Complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    8-make_multiplier.py - module for type-annotated function make_multiplier
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def mult(num: float) -> float:
+        return multiplier * num
+    return mult
