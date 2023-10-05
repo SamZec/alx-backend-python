90f5527cbb2dfd3ac2857c4b32edefddc7aaf171
SamZec <samzec4@gmail.com>
2023-10-05 16:10:49 +0000

    4. Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    4-define_variables.py - Defines and annotates the following variables
+    with the specified values
+"""
+
+
+a: int = 1
+pi: float = 3.142
+i_understand_annotations: bool = True
+school: str = 'Holberton'
