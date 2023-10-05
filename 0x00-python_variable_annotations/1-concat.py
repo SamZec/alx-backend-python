cd96fe0c9470510f2feb64e92be143787630e550
SamZec <samzec4@gmail.com>
2023-10-05 15:33:05 +0000

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""1-concat.py - a module for type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        takes a string str1 and a string str2 as arguments
+        returns a concatenated string
+    """
+    return str1 + str2
