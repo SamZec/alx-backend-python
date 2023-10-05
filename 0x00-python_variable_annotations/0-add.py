5e6817d3664596fb1bf47ef0b8d5d981a2082f65
SamZec <samzec4@gmail.com>
2023-10-05 15:19:56 +0000

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bi/env python3
+"""0-add.py - module for type-annotated function add"""
+
+
+def add(a: float, b: float) -> float:
+    """ returns the addition of two floats a and b"""
+    return a + b
