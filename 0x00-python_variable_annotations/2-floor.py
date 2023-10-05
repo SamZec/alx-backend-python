49dec59e4b23d5307196dd54fd92d81754b58e10
SamZec <samzec4@gmail.com>
2023-10-05 15:55:15 +0000

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""2-floor.py - a module for a type-annotated function floor"""
+
+
+def floor(n: float) -> int:
+    """
+        takes a float n as argument
+        returns the floor n
+    """
+    return int(n // 1)
