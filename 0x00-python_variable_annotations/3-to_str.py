68b3ad879da605a4a8cab7a5d7f120a2b5e1c9c2
SamZec <samzec4@gmail.com>
2023-10-05 16:02:32 +0000

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""3-to_str.py - a module for a type-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """
+        takes a float n as argument
+        returns the string representation n
+    """
+    return str(n)
