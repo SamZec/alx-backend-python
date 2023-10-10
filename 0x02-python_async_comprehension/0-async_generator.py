6dbce67f23229d8f5334d51d5c184e999070d98c
SamZec <samzec4@gmail.com>
2023-10-10 18:34:32 +0000

    0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""0-async_generator.py - module for async_generator function"""
+
+import asyncio
+from typing import AsyncGenerator
+import random
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+        coroutine that loop 10 times and asynchronously wait 1 second
+        yield a random number between 0 and 10.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
